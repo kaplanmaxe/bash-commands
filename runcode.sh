9ef4a2c6b5009a3b6a8d99fa4b87c2dd06925400
Max Kaplan <kaplanmaxe3@gmail.com>
2018-03-27 19:32:00 -0400

    adding runcode

---
+++ runcode.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+runcode() {
+  CONTAINER='';
+  VOLUME='';
+  COMMAND='bash';
+  ARGS=();
+
+  for i in $@; do ARGS+=($i); done;
+  for (( i=0;i<${#ARGS[@]};i++ )) do
+      case "${ARGS[i]}" in
+          node)
+              CONTAINER="node";;
+          go)
+              CONTAINER="golang";;
+          --volume)
+              DIRECTORY=${ARGS[i+1]};
+              VOLUME="-v $DIRECTORY:/app";;
+          --command)
+            COMMAND=${ARGS[i+1]};;
+          *)
+              break;;
+      esac
+  done;
+
+  if [ -z $CONTAINER ]
+  then
+      echo "No container specified!";
+      exit 1;
+  fi
+  docker run -ti --rm $VOLUME $CONTAINER $COMMAND;
+}
